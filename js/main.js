8238ae5725d73093f871b18b8811b3599f7513ea
albertogaia <alberto.gaia95@gmail.com>
2021-07-19 14:40:56 +0200

    sistemato i commenti

--- js/main.js
+++ js/main.js
@@ -1,27 +1,26 @@
 // Scrivi un programma che stampi i numeri da 1 a 100
 var items = 100;
-var numbers = [];
-var flag = false;
+var numbers = []; // preparo l'array da riempire
+var flag = false; // definisco un flag
 
 for (var i = 0; i < items; i++) {
    numbers[i] = i + 1;
 
-    
     if (numbers[i] % 5 == 0 && numbers[i] % 3 == 0) {   // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
         numbers[i] = 'FizzBuzz';
         flag = true;
     }
-    else if (numbers[i] % 3 == 0) {                 // ma per i multipli di 3 stampi “Fizz”
+    else if (numbers[i] % 3 == 0) {        // ma per i multipli di 3 stampi “Fizz”
         numbers[i] = 'Fizz';
         flag = true;
     }
-    else if (numbers[i] % 5 == 0){                  // per i multipli di 5 stampi Buzz.
+    else if (numbers[i] % 5 == 0){         // per i multipli di 5 stampi Buzz.
         numbers[i] = 'Buzz';
         flag = true;
     }
 }
 
-for (i = 0; i < items; i++) {                       // con il flag usciamo dal ciclo e cambiamo il nome con quello definito nella condizione dentro il for
+for (i = 0; i < items; i++) {             // con il flag usciamo dal ciclo e cambiamo il nome con quello definito nella condizione dentro il for
     if (flag == true){
     console.log(numbers[i])
     }
